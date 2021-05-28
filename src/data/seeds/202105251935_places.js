a2ba608f5ccbf9a05abdd2f92c8ffe43a508acd1
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-05-28 15:32:41 +0200

    Add rating to places seed

--- src/data/seeds/202105251935_places.js
+++ src/data/seeds/202105251935_places.js
@@ -7,9 +7,9 @@ module.exports = {
 
     // then add the fresh users
     await knex(tables.place).insert([
-      { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83', name: 'Loon' },
-      { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84', name: 'Dranken Geers' },
-      { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff85', name: 'Irish Pub' },
+      { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83', name: 'Loon', rating: 5 },
+      { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84', name: 'Dranken Geers', rating: 3 },
+      { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff85', name: 'Irish Pub', rating: 4 },
     ]);
   },
 };
