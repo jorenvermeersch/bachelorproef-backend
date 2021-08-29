078b479a2a50d96012087149fe0c67d321acf021
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-08-29 14:29:09 +0200

    Add Swagger docs for health routes

--- src/rest/health.js
+++ src/rest/health.js
@@ -2,11 +2,57 @@ const Router = require('@koa/router');
 const { validate, validationSchemeFactory } = require('./_validation');
 const { healthService } = require('../service');
 
+/**
+ * @swagger
+ * /api/health/ping:
+ *   get:
+ *     summary: Ping the server
+ *     tags:
+ *      - Health
+ *     responses:
+ *       200:
+ *         description: Server pongs back
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - pong
+ *               properties:
+ *                 pong:
+ *                   type: boolean
+ */
 const ping = async (ctx) => {
   ctx.sendResponse(200, healthService.ping());
 };
 ping.validationScheme = validationSchemeFactory(null);
 
+/**
+ * @swagger
+ * /api/health/version:
+ *   get:
+ *     summary: Get the server's version information
+ *     tags:
+ *      - Health
+ *     responses:
+ *       200:
+ *         description: The server's running version information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - env
+ *                 - version
+ *                 - name
+ *               properties:
+ *                 env:
+ *                   type: string
+ *                 version:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ */
 const getVersion = async (ctx) => {
   ctx.sendResponse(200, healthService.getVersion());
 };
