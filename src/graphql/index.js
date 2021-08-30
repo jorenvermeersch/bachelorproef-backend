ca148ee9087da8742b2fc83c3bf37b1cb86ec0a0
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-08-30 10:22:49 +0200

    Pretty format errors in GraphQL

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -26,6 +26,23 @@ const installApolloServer = async (app) => {
         ? ApolloServerPluginLandingPageGraphQLPlayground()
         : ApolloServerPluginLandingPageDisabled(),
     ],
+    formatError: (error) => {
+      // If we got a ServiceError, format the error
+      if (error.originalError && error.originalError.name === 'ServiceError') {
+        return {
+          ...error,
+          message: error.originalError.message,
+          extensions: {
+            code: error.originalError.code,
+            name: error.originalError.name,
+            stacktrace: error.extensions?.exception?.stacktrace || error.originalError.stack,
+            details: error.originalError.details,
+          },
+        };
+      }
+      // Otherwise, return the error as-is
+      return error;
+    },
   });
   await server.start();
   server.applyMiddleware({ app });
