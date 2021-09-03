ce613e812b9998877459e5f629f0e69f63720872
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-09-03 13:37:48 +0200

    Remove leftover data after tests

--- test/global.teardown.js
+++ test/global.teardown.js
@@ -1,8 +1,10 @@
 const { shutdownData, getKnex, tables } = require('../src/data');
 
 module.exports = async () => {
-  // Remove test user
-  await getKnex()(tables.user).where('id', '7f28c5f9-d711-4cd6-ac15-d13d71abff80').delete();
+  // Remove any leftover data
+  await getKnex()(tables.transaction).delete();
+  await getKnex()(tables.user).delete();
+  await getKnex()(tables.place).delete();
 
   // Close database connection
   await shutdownData();
