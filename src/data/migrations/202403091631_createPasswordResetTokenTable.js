3225422a3f64bfd30d31d2c086c9bf3640f1d409
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-03-10 11:18:27 +0100

    fix: wrong table name

--- src/data/migrations/202403091631_createPasswordResetTokenTable.js
+++ src/data/migrations/202403091631_createPasswordResetTokenTable.js
@@ -4,7 +4,7 @@ const uuidv4Length = 36;
 
 module.exports = {
   up: async (knex) => {
-    await knex.schema.createTable(tables.passwordResetToken, (table) => {
+    await knex.schema.createTable(tables.passwordResetRequest, (table) => {
       table.integer('user_id').unsigned().notNullable();
       table.string('token', uuidv4Length).notNullable();
       table.datetime('token_expiry').notNullable(); // Knex documentation: MySQL and MSSQL do not have useTz option.
@@ -20,6 +20,6 @@ module.exports = {
     });
   },
   down: (knex) => {
-    return knex.schema.dropTableIfExists(tables.passwordResetToken);
+    return knex.schema.dropTableIfExists(tables.passwordResetRequest);
   },
 };
