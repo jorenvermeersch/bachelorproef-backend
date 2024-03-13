d4f6623e648f06ec8c59ca66be84e71470c99316
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-03-13 16:09:10 +0100

    changed users seeds

--- src/data/seeds/202105251930_users.js
+++ src/data/seeds/202105251930_users.js
@@ -1,32 +1,40 @@
 const { tables } = require('..');
+const { hashPassword } = require('../../core/password');
 const Role = require('../../core/roles');
 
 module.exports = {
   seed: async (knex) => {
+    const validPassword = await hashPassword('toegepaste_informatica');
+    const tooShort = await hashPassword('a');
+
     await knex(tables.user).insert([
       {
         id: 1,
         name: 'Thomas Aelbrecht',
         email: 'thomas.aelbrecht@hogent.be',
-        password_hash:
-        '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
+        password_hash: validPassword,
         roles: JSON.stringify([Role.ADMIN, Role.USER]),
       },
       {
         id: 2,
         name: 'Pieter Van Der Helst',
         email: 'pieter.vanderhelst@hogent.be',
-        password_hash:
-        '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
+        password_hash: validPassword,
         roles: JSON.stringify([Role.USER]),
       },
       {
         id: 3,
         name: 'Karine Samyn',
         email: 'karine.samyn@hogent.be',
-        password_hash:
-        '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
+        password_hash: validPassword,
         roles: JSON.stringify([Role.USER]),
+      },
+      {
+        id: 4,
+        name: 'Joren Vermeersch',
+        email: 'joren.vermeersch@student.hogent.be',
+        password_hash: tooShort,
+        roles: JSON.stringify([Role.USER, Role.ADMIN]),
       },
     ]);
   },
