9d21dc538b7412af9b9b93c864a82e22f9c39eff
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-03-25 21:44:02 +0100

    fix: removed duplicate code in validate functions

--- src/core/validation/validate.js
+++ src/core/validation/validate.js
@@ -23,9 +23,11 @@ const cleanupJoiError = (error) =>
     return resultObj;
   }, {});
 
-// Create our own validator function which only takes the schema as an argument
+const REQUEST_PARTS = ['body', 'params', 'query'];
+
+// Create our own validator function which only takes the schema as an argument.
 const validate = (schema) => {
-  // Be sure we have a schema that isn't null or undefined
+  // Be sure we have a schema that isn't null or undefined.
   if (!schema) {
     schema = {
       body: {},
@@ -36,57 +38,28 @@ const validate = (schema) => {
 
   return (ctx, next) => {
     const errors = {};
-    if (schema.body) {
-      // Be sure the schema is a Joi instance
-      if (!Joi.isSchema(schema.body)) {
-        schema.body = Joi.object(schema.body);
+
+    REQUEST_PARTS.forEach((part) => {
+      if (schema[part]) {
+        // Be sure the schema is a Joi instance
+        if (!Joi.isSchema(schema[part])) {
+          schema[part] = Joi.object(schema[part]);
+        }
+
+        const { error: partErrors, value: partValue } = schema[part].validate(
+          part === 'body' ? ctx.request[part] : ctx[part],
+          JOI_OPTIONS,
+        );
+
+        if (partErrors) {
+          errors[part] = cleanupJoiError(partErrors);
+        } else {
+          part === 'body'
+            ? (ctx.request[part] = partValue)
+            : (ctx[part] = partValue);
+        }
       }
-
-      const { error: bodyErrors, value: bodyValue } = schema.body.validate(
-        ctx.request.body,
-        JOI_OPTIONS,
-      );
-
-      if (bodyErrors) {
-        errors.body = cleanupJoiError(bodyErrors);
-      } else {
-        ctx.request.body = bodyValue;
-      }
-    }
-
-    if (schema.params) {
-      // Be sure the schema is a Joi instance
-      if (!Joi.isSchema(schema.params)) {
-        schema.params = Joi.object(schema.params);
-      }
-
-      const { error: paramsErrors, value: paramsValue } =
-        schema.params.validate(ctx.params, JOI_OPTIONS);
-
-      if (paramsErrors) {
-        errors.params = cleanupJoiError(paramsErrors);
-      } else {
-        ctx.params = paramsValue;
-      }
-    }
-
-    if (schema.query) {
-      // Be sure the schema is a Joi instance
-      if (!Joi.isSchema(schema.query)) {
-        schema.query = Joi.object(schema.query);
-      }
-
-      const { error: queryErrors, value: queryValue } = schema.query.validate(
-        ctx.query,
-        JOI_OPTIONS,
-      );
-
-      if (queryErrors) {
-        errors.query = cleanupJoiError(queryErrors);
-      } else {
-        ctx.query = queryValue;
-      }
-    }
+    });
 
     if (Object.keys(errors).length) {
       ctx.throw(400, 'Validation failed, check details for more information', {
@@ -107,58 +80,24 @@ const validateAsync = (schema) => {
       query: {},
     };
   }
-
   return async (ctx, next) => {
     const errors = {};
 
-    if (schema.body) {
-      // Be sure the schema is a Joi instance.
-      if (!Joi.isSchema(schema.body)) {
-        schema.body = Joi.object(schema.body);
+    for (const part of REQUEST_PARTS) {
+      if (!Joi.isSchema(schema[part])) {
+        schema[part] = Joi.object(schema[part]);
       }
 
       try {
-        const bodyValue = await schema.body.validateAsync(
-          ctx.request.body,
+        const partValue = await schema[part].validateAsync(
+          part === 'body' ? ctx.request[part] : ctx[part],
           JOI_OPTIONS,
         );
-        ctx.request.body = bodyValue;
-      } catch (bodyErrors) {
-        errors.body = cleanupJoiError(bodyErrors);
-      }
-    }
-
-    if (schema.params) {
-      // Be sure the schema is a Joi instance.
-      if (!Joi.isSchema(schema.params)) {
-        schema.params = Joi.object(schema.params);
-      }
-
-      try {
-        const paramsValue = await schema.params.validateAsync(
-          ctx.params,
-          JOI_OPTIONS,
-        );
-        ctx.params = paramsValue;
-      } catch (paramsErrors) {
-        errors.params = cleanupJoiError(paramsErrors);
-      }
-    }
-
-    if (schema.query) {
-      // Be sure the schema is a Joi instance.
-      if (!Joi.isSchema(schema.query)) {
-        schema.query = Joi.object(schema.query);
-      }
-
-      try {
-        const queryValue = await schema.query.validateAsync(
-          ctx.query,
-          JOI_OPTIONS,
-        );
-        ctx.query = queryValue;
-      } catch (queryErrors) {
-        errors.query = cleanupJoiError(queryErrors);
+        part === 'body'
+          ? (ctx.request[part] = partValue)
+          : (ctx[part] = partValue);
+      } catch (partErrors) {
+        errors[part] = cleanupJoiError(partErrors);
       }
     }
 
