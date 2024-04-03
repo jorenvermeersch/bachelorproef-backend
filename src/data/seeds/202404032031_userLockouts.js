355be8cf8a4163b165d078f239f87ea497025d97
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-04-03 20:42:44 +0200

    added seeding for user lockouts table

---
+++ src/data/seeds/202404032031_userLockouts.js
@@ -0,0 +1,34 @@
+const { addMinutes } = require('date-fns');
+
+const { tables } = require('..');
+
+module.exports = {
+  seed: async (knex) => {
+    await knex(tables.userLockout).insert([
+      {
+        id: 1,
+        user_id: 1,
+        failed_login_attempts: 0,
+        lockout_end_time: null,
+      },
+      {
+        id: 2,
+        user_id: 2,
+        failed_login_attempts: 0,
+        lockout_end_time: null,
+      },
+      {
+        id: 3,
+        user_id: 3,
+        failed_login_attempts: 4,
+        lockout_end_time: null,
+      },
+      {
+        id: 4,
+        user_id: 4,
+        failed_login_attempts: 5,
+        lockout_end_time: addMinutes(new Date(), 5),
+      },
+    ]);
+  },
+};
