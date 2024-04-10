79db2cea486a0659a0ec23fc45260e792c0711dd
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-04-10 16:46:45 +0200

    simplified global test setup

--- test/global.setup.js
+++ test/global.setup.js
@@ -1,10 +1,8 @@
 const config = require('config');
 
-const { passwords, users } = require('./constants');
+const { users } = require('./constants');
 const { insertUsers } = require('./helpers/users');
-const { hashSecret } = require('../src/core/hashing');
 const { initializeLogging } = require('../src/core/logging');
-const Role = require('../src/core/roles');
 const { initializeData } = require('../src/data');
 
 module.exports = async () => {
@@ -17,24 +15,6 @@ module.exports = async () => {
   await initializeData();
 
   // Insert an admin and regular test user.
-
-  const passwordHash = await hashSecret(passwords.valid);
   const { admin: adminUser, test: testUser } = users;
-
-  await insertUsers([
-    {
-      id: testUser.id,
-      name: testUser.name,
-      email: testUser.email,
-      password_hash: passwordHash,
-      roles: JSON.stringify([Role.USER]),
-    },
-    {
-      id: adminUser.id,
-      name: adminUser.name,
-      email: adminUser.email,
-      password_hash: passwordHash,
-      roles: JSON.stringify([Role.ADMIN, Role.USER]),
-    },
-  ]);
+  await insertUsers([testUser, adminUser]);
 };
