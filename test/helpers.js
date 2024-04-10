88a341d25eb2a649e6a939b08618190f88097d34
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-04-10 15:01:05 +0200

    added helpers for testing password route

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,24 @@
+const { passwords } = require('./constants');
+const { hashSecret } = require('../src/core/hashing');
+const Role = require('../src/core/roles');
 const { getKnex, tables } = require('../src/data');
 
 const insertUsers = async (users) => {
+  if (!(users instanceof Array)) {
+    users = [users];
+  }
+
+  const passwordHash = await hashSecret(passwords.valid);
+  const userRole = JSON.stringify([Role.USER]);
+
+  users = users.map(({ id, name, email, password_hash, roles }) => ({
+    id,
+    name,
+    email,
+    password_hash: password_hash || passwordHash,
+    roles: roles || userRole,
+  }));
+
   const knex = getKnex();
 
   await knex(tables.user).insert(users);
@@ -22,4 +40,27 @@ const deleteUsers = async (userIds) => {
   await knex(tables.user).whereIn('id', userIds).delete();
 };
 
-module.exports = { insertUsers, deleteUsers };
+const requestReset = async (email, supertest) => {
+  const response = await supertest.post('/api/password/request-reset').send({
+    email: email,
+  });
+
+  return response;
+};
+
+// TODO: Add documentation.
+const parseResetEmail = (email) => {
+  if (!email) {
+    throw new Error('Email is required.');
+  }
+
+  const { text, to } = email;
+
+  // The origin is undefined when testing, so you can't use `text` to create an URL.
+  const params = new URLSearchParams(text.split('/')[1]);
+  const token = params.get('token');
+
+  return { token, email: to };
+};
+
+module.exports = { insertUsers, deleteUsers, requestReset, parseResetEmail };
