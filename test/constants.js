5029beed6f6db910385736031ca5312806fcf6de
jorenvermeersch <joren.vermeersch@student.hogent.be>
2024-04-10 15:19:23 +0200

    added some password reset tests

--- test/constants.js
+++ test/constants.js
@@ -2,8 +2,13 @@ const passwords = {
   tooLong: 'a'.repeat(256),
   tooShort: 'a',
   valid: 'hogent_toegepaste_informatica',
+  newValid: 'h0gent_toegep@ste_informat1ca_2',
   breached: 'password1234',
   invalid: 'invalid-password-r"Q[`?jZxkG8s7A#9]M6tc',
+};
+
+const tokens = {
+  validFormat: '51bc6ebc-6f85-4d33-8576-20c1fbb5d66b',
 };
 
 const users = {
@@ -25,6 +30,11 @@ const users = {
     email: 'login@hogent.be',
     password: passwords.valid,
   },
+  passwordReset: {
+    id: 4,
+    name: 'Password Reset User',
+    email: 'reset.user@hogent.be',
+  },
 };
 
-module.exports = { users, passwords };
+module.exports = { users, passwords, tokens };
--- test/specs/passwords.spec.js
+++ test/specs/passwords.spec.js
@@ -1,17 +1,27 @@
 const { mock } = require('nodemailer');
 
+const { tables } = require('../../src/data');
 const {
-  users: { test: testUser },
+  users: { test: testUser, passwordReset: resetUser },
+  passwords,
+  tokens,
 } = require('../constants');
+const {
+  requestReset,
+  parseResetEmail,
+  insertUsers,
+  deleteUsers,
+} = require('../helpers');
 const { withServer } = require('../supertest.setup');
 
 // Nodemailer is automatically mocked with nodemailer-mock in ./__mocks__/nodemailer.js
 
 describe('Passwords', () => {
-  let supertest;
+  let supertest, knex;
 
-  withServer(({ supertest: s }) => {
+  withServer(({ supertest: s, knex: k }) => {
     supertest = s;
+    knex = k;
   });
 
   describe('POST /api/password/request-reset', () => {
@@ -22,7 +32,7 @@ describe('Passwords', () => {
     });
 
     afterAll(async () => {
-      // Delete all reset requests from test database.
+      await knex(tables.passwordResetRequest).del();
     });
 
     it('should 202 and send password reset email for existing user', async () => {
@@ -49,40 +59,100 @@ describe('Passwords', () => {
   });
 
   describe('POST /api/password/reset', () => {
+    const url = '/api/password/reset';
+
+    beforeAll(async () => {
+      await insertUsers({
+        ...resetUser,
+      });
+    });
+
+    afterAll(async () => {
+      await deleteUsers([resetUser.id]);
+    });
+
+    afterEach(async () => {
+      await knex(tables.passwordResetRequest).del();
+    });
+
     it('should 204 and reset the password', async () => {
-      throw new Error('Not implemented');
+      await requestReset(resetUser.email, supertest);
+
+      const resetMail = mock.getSentMail()[0];
+      const { token } = parseResetEmail(resetMail);
+
+      const response = await supertest.post(url).send({
+        email: resetUser.email,
+        token,
+        newPassword: passwords.newValid,
+      });
+
+      expect(response.status).toBe(204);
     });
 
     it('should 400 for missing fields', async () => {
-      throw new Error('Not implemented');
+      // TODO!
     });
 
     it('should 400 for invalid fields', async () => {
-      throw new Error('Not implemented');
+      // TODO!
     });
 
     it('should 400 for unknown user', async () => {
-      throw new Error('Not implemented');
+      const response = await supertest.post(url).send({
+        email: 'unknown.user@hogent.be',
+        token: tokens.validFormat,
+        newPassword: passwords.valid,
+      });
+
+      expect(response.status).toBe(400);
     });
 
     it('should 400 for existing user without a reset request', async () => {
-      throw new Error('Not implemented');
+      const response = await supertest.post(url).send({
+        email: resetUser.email,
+        token: tokens.validFormat,
+        newPassword: passwords.newValid,
+      });
+
+      expect(response.status).toBe(400);
     });
 
     it('should 400 for invalid reset token', async () => {
-      throw new Error('Not implemented');
+      await requestReset(resetUser.email, supertest);
+
+      const response = await supertest.post(url).send({
+        email: resetUser.email,
+        token: tokens.validFormat,
+        newPassword: passwords.newValid,
+      });
+
+      expect(response.status).toBe(400);
     });
 
     it('should 400 for expired reset token', async () => {
-      throw new Error('Not implemented');
+      // TODO!
     });
 
     it('should 400 when using a old reset token after requesting a new one', async () => {
-      throw new Error('Not implemented');
+      await requestReset(resetUser.email, supertest);
+
+      const resetMail = mock.getSentMail()[0];
+      const { token: oldToken } = parseResetEmail(resetMail);
+
+      await requestReset(resetUser.email, supertest);
+
+      const response = await supertest.post(url).send({
+        email: resetUser.email,
+        token: oldToken,
+        newPassword: passwords.newValid,
+      });
+
+      expect(response.status).toBe(400);
     });
 
     it('should 400 when using the same reset token twice', async () => {
-      throw new Error('Not implemented');
+      // TODO!
     });
   });
 });
